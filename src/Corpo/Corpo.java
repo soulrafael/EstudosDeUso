a0fa431eae6b9ae609f927a1555deda1d9e3897a
RafaelBSoul <coffeemonstersandbooks@gmail.com>
2024-12-03 23:34:24 -0300

    Exercicios

---
+++ src/Corpo/Corpo.java
@@ -0,0 +1,31 @@
+package Corpo;
+
+public class Corpo {
+    private double altura;
+    private double peso;
+    private double largura;
+
+    public void CaloriasQueimada(){
+        
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+    public double getPeso() {
+        return peso;
+    }
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+    public double getLargura() {
+        return largura;
+    }
+    public void setLargura(double largura) {
+        this.largura = largura;
+    }
+    
+}
---
+++ src/Exercicios/Exercicios.java
@@ -0,0 +1,39 @@
+package Exercicios;
+
+public class Exercicios {
+   private String correr;
+    private String pedala;
+   private String danca;
+   private int tempo;
+    private int quantidade;
+    public String getCorrer() {
+        return correr;
+    }
+    public void setCorrer(String correr) {
+        this.correr = correr;
+    }
+    public String getPedala() {
+        return pedala;
+    }
+    public void setPedala(String pedala) {
+        this.pedala = pedala;
+    }
+    public String getDanca() {
+        return danca;
+    }
+    public void setDanca(String danca) {
+        this.danca = danca;
+    }
+    public int getTempo() {
+        return tempo;
+    }
+    public void setTempo(int tempo) {
+        this.tempo = tempo;
+    }
+    public int getQuantidade() {
+        return quantidade;
+    }
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+}
